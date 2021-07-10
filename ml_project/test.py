7cb1c22583674141a88e992ddd9a0a4be1809cdc
ayanavasarkar <f2013206@dubai.bits-pilani.ac.in>
2021-07-09 23:01:39 -0400

    Implementing the Test loop

--- ml_project/test.py
+++ ml_project/test.py
@@ -11,6 +11,20 @@ tf.disable_v2_behavior()
 import tqdm
 np.random.seed(5)
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+parser.add_argument('--train_data', type=str, default='data/train_data.dat', help='path to training data')
+parser.add_argument('--test_data', type=str, default='data/test_data.dat',help='path to test data')
+parser.add_argument('--test', type=bool, default=False, help='Test or not')
+parser.add_argument('--n_users', type=int, default=5551,help='#Users')
+parser.add_argument('--a_train', type=int, default=13584,help='#Articles train')
+parser.add_argument('--a_test', type=int, default=1584,help='#Articles test/Eval')
+
+
+args = parser.parse_args()
+
 # find vocabulary_size = 8000
 with open(r"data/vocabulary.dat") as vocabulary_file:
     vocabulary_size = len(vocabulary_file.readlines())
@@ -23,24 +37,54 @@ with open(r"data/mult.dat") as item_info_file:
 item_infomation_matrix = np.zeros((item_size, vocabulary_size))
 
 # build item_infomation_matrix
-with open(r"data/mult.dat") as item_info_file:
-    sentences = item_info_file.readlines()
+with open(r"data/mult.dat") as f:
+    s = f.readlines()
 
-    for index, sentence in enumerate(sentences):
-        words = sentence.strip().split(" ")[1:]
+    for ind, s in enumerate(s):
+        words = s.strip().split(" ")[1:]
         for word in words:
-            vocabulary_index, number = word.split(":")
-            item_infomation_matrix[index][int(vocabulary_index)] = number
+            vocab_ind, n = word.split(":")
+            item_infomation_matrix[ind][int(vocab_ind)] = n
 
-import numpy as np
-rating_matrix = load_user_article_likes('data')
+## train data user->article matching
+def load_user_article_likes(path,  num_users, num_articles):
+    """
+    Load the article likes for each user
 
-def mask(corruption_level ,size):
-    mask = np.random.binomial(1, 1 - corruption_level, [size[0],size[1]])
-    return mask
+    Keyword arguments:
+        data_folder -- the path to the folder that contains the data
+        num_users -- the number of users in the dataset - default is 5551
+        num_articles -- the number of articles in the dataset - default is 13584
 
-def add_noise(x , corruption_level ):
-    x = x * mask(corruption_level , x.shape)
+    Returns:
+        2d numpy matrix with describing user like information.
+        This matrix is num_users X num_articles.
+        There is a 1 in locations in which a user likes an article u -> a
+        citations[u][a] and 0 otherwise.
+    """
+
+    user_items = np.zeros([num_users, num_articles])
+    with open(os.path.join(path)) as users_file:
+        for i, line in enumerate(users_file.readlines()):
+            for article in line.strip().split()[1:]:
+                user_items[i][int(article)] = 1
+    return user_items
+
+
+if args.test == False:
+    rating_matrix = load_user_article_likes(args.train_data, args.n_users, args.a_train)
+    rating_matrix_test = load_user_article_likes(args.train_data, args.n_users, args.a_train)
+else:
+    rating_matrix = load_user_article_likes(args.train_data, args.n_users, args.a_train)
+    rating_matrix_test = load_user_article_likes(args.test_data, args.n_users, args.a_test)
+
+
+
+def mask(c ,size):
+    return np.random.binomial(1, 1 - c, [size[0],size[1]])
+
+def add_noise(x, c):
+    x *= mask(c, x.shape)
     return x
 
 
@@ -91,7 +135,7 @@ def recall_at_m(test_data_mat, prediction_mat, M=50) -> float:
 
 
 class CDL():
-    def __init__(self, rating_matrix, item_infomation_matrix):
+    def __init__(self, rating_matrix, rating_matrix_test, item_infomation_matrix):
 
         self.n_input = item_infomation_matrix.shape[1]
         self.n_hidden1 = 200
@@ -105,7 +149,7 @@ class CDL():
 
         self.drop_ratio = 0.1
         self.learning_rate = 0.01
-        self.epochs = 2
+        self.epochs = 1
         self.batch_size = 256
 
         self.a = 1
@@ -114,6 +158,7 @@ class CDL():
 
         self.num_u = rating_matrix.shape[0]
         self.num_v = rating_matrix.shape[1]
+        self.num_v_ = rating_matrix_test.shape[1]
 
         self.Weights = {
             'w1': tf.Variable(
@@ -135,36 +180,46 @@ class CDL():
         self.item_infomation_matrix = item_infomation_matrix
 
         self.rating_matrix = rating_matrix
+        self.rating_matrix_test = rating_matrix_test
 
         for i in range(self.num_u):
             try:
                 x = np.random.choice(np.where(self.rating_matrix[i, :] > 0)[0], self.P)
+                x_ = np.random.choice(np.where(self.rating_matrix_test[i, :] > 0)[0], self.P)
             except:
                 x = 1
+                x_ = 1
             self.rating_matrix[i, :].fill(0)
             self.rating_matrix[i, x] = 1
+            self.rating_matrix_test[i, :].fill(0)
+            self.rating_matrix_test[i, x_] = 1
 
         self.confidence = np.mat(np.ones(self.rating_matrix.shape)) * self.b
         self.confidence[np.where(self.rating_matrix > 0)] = self.a
+        self.confidence_test = np.mat(np.ones(self.rating_matrix_test.shape)) * self.b
+        self.confidence_test[np.where(self.rating_matrix_test > 0)] = self.a
 
     def encoder(self, x, drop_ratio):
-        w1, b1 = self.Weights['w1'], self.Biases['b1']
+        w1 = self.Weights['w1']
+        b1 = self.Biases['b1']
         L1 = tf.nn.sigmoid(tf.matmul(x, w1) + b1)
         L1 = tf.nn.dropout(L1, keep_prob=1 - drop_ratio)
 
-        w2, b2 = self.Weights['w2'], self.Biases['b2']
-
+        w2 = self.Weights['w2']
+        b2 = self.Biases['b2']
         L2 = tf.nn.sigmoid(tf.matmul(L1, w2) + b2)
         L2 = tf.nn.dropout(L2, keep_prob=1 - drop_ratio)
 
         return L2
 
     def decoder(self, x, drop_ratio):
-        w3, b3 = self.Weights['w3'], b2 = self.Biases['b3']
+        w3 = self.Weights['w3']
+        b3 = self.Biases['b3']
         L3 = tf.nn.sigmoid(tf.matmul(x, w3) + b3)
         L3 = tf.nn.dropout(L3, keep_prob=1 - drop_ratio)
 
-        w4, b4 = self.Weights['w4'], self.Biases['b4']
+        w4 = self.Weights['w4']
+        b4 = self.Biases['b4']
         L4 = tf.nn.sigmoid(tf.matmul(L3, w4) + b4)
         L4 = tf.nn.dropout(L4, keep_prob=1 - drop_ratio)
 
@@ -241,28 +296,30 @@ class CDL():
 
         start_time = time.time()
 
-        random_idx = np.random.permutation(self.num_v)
+        random_idx = np.random.permutation(self.num_v_)
 
         self.item_infomation_matrix_noise = add_noise(self.item_infomation_matrix, 0.3)
 
-        batch_cost = 0
+        total_op = np.zeros((self.num_u, self.num_v_))
         for i in range(0, self.item_infomation_matrix.shape[0], self.batch_size):
             batch_idx = random_idx[i:i + self.batch_size]
-            _, loss = self.sess.run([self.optimizer, self.loss],
+            op = self.sess.run([self.sdae_output],
                                     feed_dict={self.X_0: self.item_infomation_matrix_noise[batch_idx, :],
                                                self.X_c: self.item_infomation_matrix[batch_idx, :],
-                                               self.R: self.rating_matrix[:, batch_idx],
-                                               self.C: self.confidence[:, batch_idx],
+                                               self.R: self.rating_matrix_test[:, batch_idx],
+                                               self.C: self.confidence_test[:, batch_idx],
                                                self.drop_ratio: 0.1,
                                                self.model_batch_data_idx: batch_idx})
-            batch_cost = batch_cost + loss
+            print(op.shape)
+            break
 
-        print ("Test //",  " Total Loss = {:.2f}".format(batch_cost),
-               "Elapsed time : %d sec" % (time.time() - start_time))
+        # print ("Test //",  " Total Loss = {:.2f}".format(batch_cost),
+        #        "Elapsed time : %d sec" % (time.time() - start_time))
 
-    return self.sess.run((tf.matmul(self.U, self.V, transpose_b=True)))
+        return self.sess.run((tf.matmul(self.U, self.V, transpose_b=True)))
 
 R_train = rating_matrix.copy()
-cdl = CDL(R_train , item_infomation_matrix)
+cdl = CDL(R_train, rating_matrix_test, item_infomation_matrix)
 cdl.build_model()
 R = cdl.train_model()
+r_ = cdl.test_model()
